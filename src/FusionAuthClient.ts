2e9c8bf482fafea341ebcb5d829985265daef589
Daniel DeGroff <daniel@fusionauth.io>
2020-02-26 16:31:32 -0700

    Move Verification Keys for the ExternalJWTIdentityProvider to Key Master

--- src/FusionAuthClient.ts
+++ src/FusionAuthClient.ts
@@ -1,18 +1,18 @@
 /*
-* Copyright (c) 2019, FusionAuth, All Rights Reserved
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*   http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing,
-* software distributed under the License is distributed on an
-* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
-* either express or implied. See the License for the specific
-* language governing permissions and limitations under the License.
-*/
+ * Copyright (c) 2019, FusionAuth, All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ */
 
 import IRESTClient from "./IRESTClient"
 import DefaultRESTClientBuilder from "./DefaultRESTClientBuilder";
@@ -4194,9 +4194,10 @@ export interface ExternalJWTApplicationConfiguration extends BaseIdentityProvide
  */
 export interface ExternalJWTIdentityProvider extends BaseIdentityProvider<ExternalJWTApplicationConfiguration> {
   claimMap?: Map<string, string>;
+  defaultKeyId?: UUID;
   domains?: Set<string>;
   headerKeyParameter?: string;
-  keys?: Map<string, string>;
+  keyIds?: Set<UUID>;
   oauth2?: IdentityProviderOauth2Configuration;
   uniqueIdentityClaim?: string;
 }
