c38f4106c2e9bef916154f242a39b40ce59a4454
Daniel DeGroff <daniel@fusionauth.io>
2020-02-26 20:15:41 -0700

    Sync domain / clients

--- src/FusionAuthClient.ts
+++ src/FusionAuthClient.ts
@@ -3762,9 +3762,9 @@ export interface CleanSpeakConfiguration extends Enableable {
 }
 
 export enum ClientAuthenticationMethod {
-  none = "None (none)",
-  client_secret_basic = "HTTP Basic authentication (client_secret_basic)",
-  client_secret_post = "Client credentials in the request body (client_secret_post)"
+  none,
+  client_secret_basic,
+  client_secret_post
 }
 
 /**
