3b579611e7619e0ee9b22a7dbfe7bfd6daa94328
FusionAuth Release <dev@fusionauth.io>
2022-11-14 22:31:47 +0000

    Updated version for fusionauth-typescript-client to 1.41.1

--- src/FusionAuthClient.ts
+++ src/FusionAuthClient.ts
@@ -7832,17 +7832,17 @@ export interface Lambda {
 }
 
 export interface LambdaConfiguration {
+  reconcileId?: UUID;
+}
+
+export interface LambdaConfiguration {
+  reconcileId?: UUID;
+}
+
+export interface LambdaConfiguration {
   accessTokenPopulateId?: UUID;
   idTokenPopulateId?: UUID;
   samlv2PopulateId?: UUID;
-}
-
-export interface LambdaConfiguration {
-  reconcileId?: UUID;
-}
-
-export interface LambdaConfiguration {
-  reconcileId?: UUID;
 }
 
 /**
