983a64e401935441cdc46c8b915c5b7a72d6a733
Lyle Schemmerling <lshem16@gmail.com>
2025-03-06 08:58:42 -0700

    1.56.0 client updates

--- src/FusionAuthClient.ts
+++ src/FusionAuthClient.ts
@@ -1,18 +1,18 @@
 /*
-* Copyright (c) 2019-2023, FusionAuth, All Rights Reserved
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*   http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing,
-* software distributed under the License is distributed on an
-* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
-* either express or implied. See the License for the specific
-* language governing permissions and limitations under the License.
-*/
+ * Copyright (c) 2019-2025, FusionAuth, All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ */
 
 import IRESTClient from "./IRESTClient"
 import DefaultRESTClientBuilder from "./DefaultRESTClientBuilder";
@@ -10071,6 +10071,7 @@ export interface UserLoginIdDuplicateOnUpdateEvent extends UserLoginIdDuplicateO
  * @author Brett Pontarelli
  */
 export interface TenantSSOConfiguration {
+  allowAccessTokenBootstrap?: boolean;
   deviceTrustTimeToLiveInSeconds?: number;
 }
 
@@ -11578,7 +11579,9 @@ export interface APIKey {
   keyManager?: boolean;
   lastUpdateInstant?: number;
   metaData?: APIKeyMetaData;
+  name?: string;
   permissions?: APIKeyPermissions;
+  retrievable?: boolean;
   tenantId?: UUID;
 }
 
