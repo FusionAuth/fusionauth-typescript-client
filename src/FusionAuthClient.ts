704e7bd0ffd5323dda10a60f84138e8a849a85c3
Daniel DeGroff <daniel@fusionauth.io>
2025-09-08 04:48:12 -0600

    domain sync

--- src/FusionAuthClient.ts
+++ src/FusionAuthClient.ts
@@ -7988,7 +7988,7 @@ export interface GoogleIdentityProviderProperties {
 }
 
 /**
- * Authorization Grant types as defined by the <a href="https://tools.ietf.org/html/rfc6749">The OAuth 2.0 Authorization
+ * Authorization Grant types as defined by the <a href="https://tools.ietf.org/html/rfc6749">OAuth 2.0 Authorization
  * Framework - RFC 6749</a>.
  * <p>
  * Specific names as defined by <a href="https://tools.ietf.org/html/rfc7591#section-4.1">
