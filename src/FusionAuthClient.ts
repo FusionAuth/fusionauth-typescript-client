a2626f9fe423c31215d48e53de6e5dfa312f4322
Brady Wied <brady.wied@fusionauth.io>
2025-09-12 11:28:09 -0600

    add new overload case

--- src/FusionAuthClient.ts
+++ src/FusionAuthClient.ts
@@ -257,13 +257,33 @@ export class FusionAuthClient {
    * 
    * An HTTP status code of 400 with a general error code of [TrustTokenRequired] indicates that a Trust Token is required to make a POST request to this API.
    *
-   * @param {string} loginId The loginId of the User that you intend to change the password for.
+   * @param {string} loginId The loginId (email or username) of the User that you intend to change the password for.
    * @returns {Promise<ClientResponse<void>>}
    */
   checkChangePasswordUsingLoginId(loginId: string): Promise<ClientResponse<void>> {
     return this.start<void, Errors>()
         .withUri('/api/user/change-password')
-        .withParameter('username', loginId)
+        .withParameter('loginId', loginId)
+        .withMethod("GET")
+        .go();
+  }
+
+  /**
+   * Check to see if the user must obtain a Trust Request Id in order to complete a change password request.
+   * When a user has enabled Two-Factor authentication, before you are allowed to use the Change Password API to change
+   * your password, you must obtain a Trust Request Id by completing a Two-Factor Step-Up authentication.
+   * 
+   * An HTTP status code of 400 with a general error code of [TrustTokenRequired] indicates that a Trust Token is required to make a POST request to this API.
+   *
+   * @param {string} loginId The loginId of the User that you intend to change the password for.
+   * @param {Array<String>} loginIdTypes the identity types that FusionAuth will compare the loginId to.
+   * @returns {Promise<ClientResponse<void>>}
+   */
+  checkChangePasswordUsingLoginIdAndLoginIdTypes(loginId: string, loginIdTypes: Array<String>): Promise<ClientResponse<void>> {
+    return this.start<void, Errors>()
+        .withUri('/api/user/change-password')
+        .withParameter('loginId', loginId)
+        .withParameter('loginIdTypes', loginIdTypes)
         .withMethod("GET")
         .go();
   }
